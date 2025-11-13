acce8a93cc3b4e3c98c3fbf76927643dd2571b75
jemin cha <cjm723@naver.com>
2025-11-13 19:23:39 +0900

    feat(prg-01): Implement basic GET request routing

--- lec-06-prg-01-http-web-server.js
+++ lec-06-prg-01-http-web-server.js
@@ -7,12 +7,28 @@ const serverPort = 8080;
 
 // 3. HTTP 서버 생성
 const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        console.log("## do_GET() activated.");
+    
     // 4. HTTP 응답 헤더 작성
     res.writeHead(200, { 'Content-Type': 'text/html' });
     
-    // 5. HTTp 응답 본문 작성, 전송 완료
-    res.end('<html><body>Hello World from Node.js HTTP Server!</body></html>');
+        if (req.url.includes('?')) {
+        // Query parameter 있는 경우 계산 요청
+        res.end('<html><body>Query parameter logic (ToDo)</body></html>');
 
+        } else {
+        // Query parameter 없는 경우 단순 경로 요청
+        // 5. HTTp 응답 본문
+        const responseMessage = `<p>HTTP Request GET for Path: ${req.url}</p>`;
+        res.end(`<html><body>${responseMessage}</body></html>`);
+        console.log(`## GET request for directory => ${req.url}.`);
+    }
+   
+    } else {
+    // GET 아닌 요청 => 일단 거부
+    console.log(`This server only handles GET request for now`);
+    }
 });
 
 // 6. 지정된 포트 번호와 이름으로 서버 실행
