8f9d7c91005dc4e97e55c90da6b09c66c0e8d73e
jemin cha <cjm723@naver.com>
2025-11-23 17:14:02 +0900

    feat(prg-05,06): Implement JSON stringify and parse examples

---
+++ lec-06-prg-06-json-example.js
@@ -0,0 +1,50 @@
+const superHeroesSource = {
+    "squadName": "Super hero squad",
+  "homeTown": "Metro City",
+  "formed": 2016,
+  "secretBase": "Super tower",
+  "active": true,
+  "members": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Inferno",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+};
+
+// 1. 객체 -> JSON 문자열 변환
+const superHeroesMid = JSON.stringify(superHeroesSource, null, 4);
+
+// 2. JSON 문자열 -> 객체 파싱
+const superHeroes = JSON.parse(superHeroesMid);
+
+// 3. 데이터 접근 확인
+console.log(superHeroes['homeTown']);
